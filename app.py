b8ce8e27ea0fc8175dd881e812e8567a271d17d1
Panda <aviral19rana@gmail.com>
2020-09-23 14:39:53 +0530

    added graph

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import os
 from flask import Flask, request, redirect, url_for, render_template, jsonify
 import base64
-import time
 ###
 from tensorflow.keras.models import load_model
 import numpy as np
 from skimage import data, color, io
 from skimage.transform import rescale, resize
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import math
 ###
 
-UPLOAD_FOLDER = f'{os.getcwd()}/uploads'
+UPLOAD_FOLDER = f'{os.getcwd()}/static/upload'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -21,11 +21,19 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def upload_file():
     return render_template("index.html")
 
-
+nseed = 10
+tot_items = len(os.listdir(UPLOAD_FOLDER))
 @app.route('/data', methods=['POST'])
 def create_entry():
-
+    global nseed, tot_items
     # POST request
+    tot_items = len(os.listdir(UPLOAD_FOLDER))
+    print("tot_items: ", tot_items)
+    if tot_items >= 5:
+        tot_items = 0
+        items_inside = os.listdir(UPLOAD_FOLDER)
+        for ele in items_inside:
+            os.remove(UPLOAD_FOLDER + "/" + ele)
     if request.method == 'POST':
         # print('Incoming..')
         req = request.get_json(force=True)
@@ -58,10 +66,13 @@ def create_entry():
         ######
         ret_val = answer.argmax()
         print(ret_val)
-        vals = [x for x in answer[0]]
         ll = []
+        name = "x"
         try:
-            ll = [round(x*100) for x in answer[0]]
+            name = str(nseed)+ ".png"
+            nseed+=1
+            ll = [round(x*100, 2) for x in answer[0]]
+            savePlot(ll, name)
             print(ll)
             conf = ll[ret_val]
             print("Conf: ", conf)
@@ -70,13 +81,27 @@ def create_entry():
 
         #####
         # return str(ret_val)
-        p = {"number": str(ret_val), "predictions": ll}
+        p = {"number": str(ret_val), "predictions": ll, "link": name}
         return jsonify(p)
     else:
         message = {'message':'Had some error'}
         return jsonify(message)
 
 
+def savePlot(ll, name):
+    fig, ax = plt.subplots(nrows=1, ncols=1)  # create figure & 1 axis
+    ax.bar([str(x) for x in range(10)], ll, color="#ff660d")
+    plt.xlabel('Digits')
+    plt.ylabel('Confidence in %')
+    plt.title('Confidence Plot for the prediction')
+    fig.patch.set_facecolor((240/255, 240/255, 240/255))
+    ax.set_facecolor((240/255, 240/255, 240/255))
+    xlocs = [i for i in range(10)]
+    for i, v in enumerate(ll):
+        if int(v) != 0:
+            plt.text(xlocs[i]-0.20, v + 0.3, str(v)+"%")
+    fig.savefig(f'static/upload/{name}')  # save the figure to file
+    plt.close(fig)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- extra.py
+++ extra.py
@@ -1,5 +1,17 @@
-import matplotlib.pyplot as plt
-fig, ax = plt.subplots( nrows=1, ncols=1 )  # create figure & 1 axis
-ax.bar(list(range(10)), list(range(10,20)))
-fig.savefig('to.png')   # save the figure to file
-plt.close(fig)
+# import matplotlib.pyplot as plt
+# def savePlot(ll):
+#     fig, ax = plt.subplots(nrows=1, ncols=1)  # create figure & 1 axis
+#     ax.bar([str(x) for x in range(10)], ll, color="#ff660d")
+#     plt.xlabel('Digits')
+#     plt.ylabel('Confidence in %')
+#     plt.title('Confidence Plot for the prediction')
+#     fig.patch.set_facecolor((240/255, 240/255, 240/255))
+#     ax.set_facecolor((240/255, 240/255, 240/255))
+#     xlocs = [i for i in range(10)]
+#     for i, v in enumerate(ll):
+#         if int(v) != 0:
+#             plt.text(xlocs[i]-0.20, v + 0.3, str(v)+"%")
+#     fig.savefig('base_plot.png')  # save the figure to file
+#     plt.close(fig)
+#
+# savePlot([0,0,0,0,0,0,0,0,0,0])
